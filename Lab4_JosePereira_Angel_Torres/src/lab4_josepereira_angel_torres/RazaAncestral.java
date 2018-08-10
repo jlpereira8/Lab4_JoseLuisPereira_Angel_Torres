497df9c56ac565d3c6677a48567364938e37586f
antox89 <ator89@unitec.edu>
2018-08-10 13:45:14 -0600

    RazaAncestral Clase

---
+++ Lab4_JosePereira_Angel_Torres/src/lab4_josepereira_angel_torres/RazaAncestral.java
@@ -0,0 +1,13 @@
+
+
+package lab4_josepereira_angel_torres;
+
+
+public abstract class RazaAncestral {
+    
+    public abstract boolean moverse();
+    public abstract boolean atacar();
+    
+}
+
+

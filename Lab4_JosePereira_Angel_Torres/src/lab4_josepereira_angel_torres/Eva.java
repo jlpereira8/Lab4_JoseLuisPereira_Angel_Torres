6836bf111b73f2f4766afbdaae3441481815ca02
unknown <you@example.com>
2018-08-10 13:33:29 -0600

    primer push

---
+++ Lab4_JosePereira_Angel_Torres/src/lab4_josepereira_angel_torres/Eva.java
@@ -0,0 +1,121 @@
+/*
+ * Color
+-Año de producción
+-Altura ( en metros )
+-Alma (La persona que “ofreció” su alma solo se requiere el nombre)
+-Numero de ojos
+-País donde fue producido
+-Piloto
+-Código de serie
+-Porcentaje de daños
+
+ */
+package lab4_josepereira_angel_torres;
+
+import java.util.Date;
+
+public class Eva {
+    String Color;
+    Date fecha;
+    double altura;
+    String alma;
+    int num_ojos;
+    String pais;
+    Piloto piloto;
+    String codigo;
+    double porcentaje_dano;
+
+    public Eva() {
+    }
+
+    public Eva(String Color, Date fecha, double altura, String alma, int num_ojos, String pais, Piloto piloto, String codigo, double porcentaje_dano) {
+        this.Color = Color;
+        this.fecha = fecha;
+        this.altura = altura;
+        this.alma = alma;
+        this.num_ojos = num_ojos;
+        this.pais = pais;
+        this.piloto = piloto;
+        this.codigo = codigo;
+        this.porcentaje_dano = porcentaje_dano;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getAlma() {
+        return alma;
+    }
+
+    public void setAlma(String alma) {
+        this.alma = alma;
+    }
+
+    public int getNum_ojos() {
+        return num_ojos;
+    }
+
+    public void setNum_ojos(int num_ojos) {
+        this.num_ojos = num_ojos;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Piloto getPiloto() {
+        return piloto;
+    }
+
+    public void setPiloto(Piloto piloto) {
+        this.piloto = piloto;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPorcentaje_dano() {
+        return porcentaje_dano;
+    }
+
+    public void setPorcentaje_dano(double porcentaje_dano) {
+        this.porcentaje_dano = porcentaje_dano;
+    }
+
+    @Override
+    public String toString() {
+        return "Eva{" + "Color=" + Color + ", fecha=" + fecha + ", altura=" + altura + ", alma=" + alma + ", num_ojos=" + num_ojos + ", pais=" + pais + ", piloto=" + piloto + ", codigo=" + codigo + ", porcentaje_dano=" + porcentaje_dano + '}';
+    }
+    
+    
+}
---
+++ Lab4_JosePereira_Angel_Torres/src/lab4_josepereira_angel_torres/Piloto.java
@@ -0,0 +1,101 @@
+/*
+ * -Nombre
+-Edad
+-Nombre del familiar cercano
+-Encargado en Nerv
+-Escuela a la que asiste
+-Porcentaje de Sincronización con el EVA
+-Eva asignado
+
+ */
+package lab4_josepereira_angel_torres;
+
+/**
+ *
+ * @author j0c3lwiz
+ */
+public class Piloto {
+    String Nombre;
+    String edad;
+    String nombre_familiar;
+    String encargado_nerv;
+    String escuela;
+    double sincronizacion;
+    Eva eva_asignado;
+
+    public Piloto() {
+    }
+
+    public Piloto(String Nombre, String edad, String nombre_familiar, String encargado_nerv, String escuela, double sincronizacion, Eva eva_asignado) {
+        this.Nombre = Nombre;
+        this.edad = edad;
+        this.nombre_familiar = nombre_familiar;
+        this.encargado_nerv = encargado_nerv;
+        this.escuela = escuela;
+        this.sincronizacion = sincronizacion;
+        this.eva_asignado = eva_asignado;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre_familiar() {
+        return nombre_familiar;
+    }
+
+    public void setNombre_familiar(String nombre_familiar) {
+        this.nombre_familiar = nombre_familiar;
+    }
+
+    public String getEncargado_nerv() {
+        return encargado_nerv;
+    }
+
+    public void setEncargado_nerv(String encargado_nerv) {
+        this.encargado_nerv = encargado_nerv;
+    }
+
+    public String getEscuela() {
+        return escuela;
+    }
+
+    public void setEscuela(String escuela) {
+        this.escuela = escuela;
+    }
+
+    public double getSincronizacion() {
+        return sincronizacion;
+    }
+
+    public void setSincronizacion(double sincronizacion) {
+        this.sincronizacion = sincronizacion;
+    }
+
+    public Eva getEva_asignado() {
+        return eva_asignado;
+    }
+
+    public void setEva_asignado(Eva eva_asignado) {
+        this.eva_asignado = eva_asignado;
+    }
+
+    @Override
+    public String toString() {
+        return "Piloto{" + "Nombre=" + Nombre + ", edad=" + edad + ", nombre_familiar=" + nombre_familiar + ", encargado_nerv=" + encargado_nerv + ", escuela=" + escuela + ", sincronizacion=" + sincronizacion + ", eva_asignado=" + eva_asignado + '}';
+    }
+    
+    
+}
